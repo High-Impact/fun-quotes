2889287927a28b83f42549d9edf58c0816453e47
High-Impact <43792157+High-Impact@users.noreply.github.com>
2020-11-08 09:14:18 -0600

    Final
    Done.

--- components/Quotes.vue
+++ components/Quotes.vue
@@ -2,15 +2,15 @@
     <div>
         <div v-if="loading && loading !== 'error'">Loading...</div>
         <section v-else class="container mx-auto pb-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-            <div v-for="quote in quotes" class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div v-for="quote in quotes" class="rounded overflow-hidden shadow-lg bg-white border-4 border-blue-700 w-full">
                 <div class="px-6 py-4">
-                    <p class="text-gray-700 text-base">
+                    <p class="text-blue-900 text-lg">
                         {{ quote.quote }}
                     </p>
                 </div>
-                <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-                        {{ quote.dateSeen }}
+                <div class="px-6 pt-4 pb-2 opacity-50">
+                    <span class="inline-block rounded-full px-3 py-1 text-xs font-semibold text-blue-700 border-2 border-blue-700 mr-2 mb-2">
+                        Seen: {{ quote.dateSeen }}
                     </span>
                 </div>
             </div>
